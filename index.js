7ddc6114ac0add7e1e03358143bc096af7331930
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2024-03-08 11:32:22 -0600

    Added routes for different http methods

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World Paty!')
-  })
-  
-  app.listen(port, () => {
+})
+
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request Paty')
+})
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user paty')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user paty')
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
+
+  
